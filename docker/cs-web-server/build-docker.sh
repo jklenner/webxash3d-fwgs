3a798366f083a10c2157922993cbdbcb38177e73
jklenner <j@k.de>
2025-08-23 00:48:21 +0000

    added custom start page

--- docker/cs-web-server/build-docker.sh
+++ docker/cs-web-server/build-docker.sh
@@ -6,13 +6,17 @@ if [ -z "$1" ]; then
     echo ""
     echo "Description:"
     echo "  Build a Docker image for cs-web-server with the specified tag."
+    echo "  Also tags the image as 'latest'."
     echo ""
     echo "Example:"
     echo "  $0 v1.0.0"
     exit 1
 fi
 
-TAG=$1
+TAG="$1"
 
-# Run docker buildx with the provided tag
-docker buildx build --platform linux/386 -t cs-web-server:$TAG .
+# Run docker buildx with the provided tag and 'latest'
+docker buildx build --platform linux/386 \
+  -t cs-web-server:"$TAG" \
+  -t cs-web-server:latest \
+  .
